8e5aec82866e83c2a1a4a608644458c93d86a43e
Ben Eggers <beggerss@cs.washington.edu>
2014-05-08 22:45:11 -0700

    added test and standardized test output format

--- huffTree_test.go
+++ huffTree_test.go
@@ -18,8 +18,8 @@ import (
 func TestMakeTreeFromTextEmpty(t *testing.T) {
 	b := make([]byte, 0)
 	err := ioutil.WriteFile(".test", b, 0644)
-	defer os.Remove(".test")
 	if err != nil { t.Error(err) }
+	defer os.Remove(".test")
 	tree, err := makeTreeFromText(".test")
 	if err != nil {
 		t.Error("Got non-nil error from makeTreeFromText: ", err)
@@ -29,6 +29,23 @@ func TestMakeTreeFromTextEmpty(t *testing.T) {
 	}
 }
 
+func TestMakeTreeFromTextSingleChar(t *testing.T) {
+	b := []byte{255}
+	err := ioutil.WriteFile(".test", b, 0644)
+	if err != nil { t.Error(err) }
+	defer os.Remove(".test")
+	tree, err := makeTreeFromText(".test")
+	if err != nil {
+		t.Error("Got non-nil error from makeTreeFromText: ", err)
+	}
+	if tree == nil {
+		t.Error("Tree should be nil! Got: ", tree)
+	}
+	if tree.count != 1 || tree.char != 255 {
+		t.Error("Tree was built improperly! Expected: { char: 255, count: 1 },",
+			"got { char:", tree.char, ", count:", tree.count, "}")
+	}
+}
 
 ////////////////////////////////////////////////////////////////////////////////
 // makeTreeFromNodeSlice tests
@@ -43,56 +60,55 @@ func TestMakeTreeFromNodesEmpty(t *testing.T) {
 }
 
 func TestMakeTreeFromNodesOneNode(t *testing.T) {
-	node := &huffNode{char: 'x', count: 10}
+	node := &huffNode{char: 120, count: 10}
 	nodes := []*huffNode{node}
-	tree := huffNode(*makeTreeFromNodeSlice(nodes))
-	if tree.char != 'x' || tree.count != 10 {
-		t.Error("Unexpected! Got node with count: ", tree.count, " and char: ",
-			tree.char, "instead of count: ", node.count,
-			" and char: ", node.char, ".")
+	tree := makeTreeFromNodeSlice(nodes)
+	if tree.char != 120 || tree.count != 10 {
+		t.Error("Unexpected! Got node with count:", tree.count, "and char:",
+			tree.char, "instead of count:", node.count, "and char:", node.char)
 	}
 }
 
 func TestMakeTreeFromNodesBasicTree(t *testing.T) {
-	nodes := []*huffNode{{char: 'x', count: 2}, {char: 'x', count: 2}}
-	tree := huffNode(*makeTreeFromNodeSlice(nodes))
+	nodes := []*huffNode{{char: 120, count: 2}, {char: 120, count: 2}}
+	tree := makeTreeFromNodeSlice(nodes)
 	if tree.count != 4 {
 		t.Error("Tree root count should have been 4, was: ", tree.count, ".")
 	}
-	if tree.left.count != 2 || tree.left.char != 'x' {
-		t.Error("Tree's left node was wrong! Expected {'x', 2}, got {'",
-			tree.left.char, "', ", tree.left.count, "}")
+	if tree.left.count != 2 || tree.left.char != 120 {
+		t.Error("Tree's left node was wrong! Expected { 120, 2 }, got {",
+			tree.left.char, ",", tree.left.count, "}")
 	}
-	if tree.right.count != 2 || tree.right.char != 'x' {
-		t.Error("Tree's right node was wrong! Expected {'x', 2}, got {'",
-			tree.right.char, "', ", tree.right.count, "}")
+	if tree.right.count != 2 || tree.right.char != 120 {
+		t.Error("Tree's right node was wrong! Expected { 120, 2 }, got {",
+			tree.right.char, ",", tree.right.count, "}")
 	}
 }
 
 // This test is fairly tied to the implementation, but tests of something
 // internal (like this) often have to be :(
 func TestMakeTreeFromNodesMultiLevelTree(t *testing.T) {
-	nodes := []*huffNode{{char: 'x', count: 2},
-		{char: 'x', count: 2},
-		{char: 'y', count: 3}}
-	tree := huffNode(*makeTreeFromNodeSlice(nodes))
+	nodes := []*huffNode{{char: 120, count: 2},
+		{char: 120, count: 2},
+		{char: 121, count: 3}}
+	tree := makeTreeFromNodeSlice(nodes)
 	if tree.count != 7 {
-		t.Error("Tree root count should have been 7, was: ", tree.count, ".")
+		t.Error("Tree root count should have been 7, was:", tree.count)
 	}
-	if tree.left.count != 3 || tree.left.char != 'y' {
-		t.Error("Tree's left node was wrong! Expected {'y', 3}, got {'",
-			tree.left.char, "', ", tree.left.count, "}")
+	if tree.left.count != 3 || tree.left.char != 121 {
+		t.Error("Tree's left node was wrong! Expected { 121, 3 }, got {",
+			tree.left.char, ", ", tree.left.count, "}")
 	}
 	if tree.right.count != 4 {
-		t.Error("Right subtree count should have been 4, was: ", tree.right.count, ".")
+		t.Error("Right subtree count should have been 4, was:", tree.right.count)
 	}
-	if tree.right.right.count != 2 || tree.right.right.char != 'x' {
-		t.Error("Tree's right node's right node was wrong! Expected {'x', 2}, got {'",
-			tree.right.right.char, "', ", tree.right.right.count, "}")
+	if tree.right.right.count != 2 || tree.right.right.char != 120 {
+		t.Error("Tree's right node's right node was wrong! Expected { 120, 2 }, got {",
+			tree.right.right.char, ",", tree.right.right.count)
 	}
-	if tree.right.left.count != 2 || tree.right.left.char != 'x' {
+	if tree.right.left.count != 2 || tree.right.left.char != 120 {
 		t.Error("Tree's right node's left node left node was wrong!",
-			" Expected {'x', 2}, got {'", tree.right.left.char,
-			"', ", tree.right.left.count, "}")
+			"Expected { 120, 2 }, got { ", tree.right.left.char,
+			",", tree.right.left.count, "}")
 	}
 }
