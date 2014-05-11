3fc79f5ab704f2680fef2f7c0357a6c7369c498d
Ben Eggers <beggerss@cs.washington.edu>
2014-05-10 20:50:55 -0700

    wrote main.go (or sort of did..)

---
+++ main.go
@@ -0,0 +1,10 @@
+// Ben Eggers
+// GNU GPL'd
+
+package main
+
+import "huffman"
+
+// This will be the actual program to use the huffman tree to encode and decode
+// files. It will accept a command-line argument giving it a file name, and 
+// flags to tell it what to with that file (encode, or decode).
