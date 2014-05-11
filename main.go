c3798db3a2f85ff0a0b593cf7d6add3c12255f48
Ben Eggers <beggerss@cs.washington.edu>
2014-05-10 21:34:45 -0700

    usage method

--- main.go
+++ main.go
@@ -3,8 +3,25 @@
 
 package main
 
-import "huffman"
+import (
+	"os"
+	"fmt"
+)
 
 // This will be the actual program to use the huffman tree to encode and decode
 // files. It will accept a command-line argument giving it a file name, and 
 // flags to tell it what to with that file (encode, or decode).
+
+func main() {
+	args := os.Args
+	if len(args) != 4 {
+		Usage(args[0])
+	}
+}
+
+// Prints out the usage of the program, and exits with an error status. Called
+// if the user puts in bad command-line args.
+func Usage(progName string) {
+	fmt.Println("Usage:", progName, "-[e | d] FROM_FILE TO_FILE")
+	os.exit(1)
+}
