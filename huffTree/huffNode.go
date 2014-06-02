088d759b5189044c29d176bd79ca4a47f73a12b3
Ben Eggers <beggerss@cs.washington.edu>
2014-06-01 19:05:40 -0700

    go fmt

--- huffTree/huffNode.go
+++ huffTree/huffNode.go
@@ -5,7 +5,7 @@ package huffTree
 
 // Node used in the Huffman Tree
 type huffNode struct {
-	char  byte
-	count uint32
-	left, right  *huffNode
+	char        byte
+	count       uint32
+	left, right *huffNode
 }
